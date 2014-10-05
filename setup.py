bab4db9afce30dac31c82022554adb8f3945629a
Max Kaye <m@xk.io>
2014-10-05 22:18:15 +1100

    version bump for TLS

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='WSSTT',
-      version='0.0.3',
+      version='0.0.4',
       description='P2P Message Transport Layer via WebSockets',
       author='Max Kaye',
       author_email='max@eudemonia.io',
